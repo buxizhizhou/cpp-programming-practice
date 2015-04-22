692da65b14048ff0f6737063ee215690794c160c
buxizhizhou <buxizhizhou530@gmail.com>
2015-04-22 09:47:38 +0800

    add programming-homework W3

---
+++ CPP程序设计实习/W3-编程作业-魔兽世界之1-备战/W3-编程作业-魔兽世界之1-备战/stdafx.cpp
@@ -0,0 +1,8 @@
+// stdafx.cpp : source file that includes just the standard includes
+// W3-编程作业-魔兽世界之1-备战.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
